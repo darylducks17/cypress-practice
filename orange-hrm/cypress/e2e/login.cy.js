cebf1a1844cdbd38eecd00b5a804c82186864ef0
Daryl <djohns@bipsync.com>
2024-08-15 15:54:15 +0100

    another commit

--- orange-hrm/cypress/e2e/login.cy.js
+++ orange-hrm/cypress/e2e/login.cy.js
@@ -1,5 +1,13 @@
 describe('login', () => {
   it('successful login', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+    cy.fixture("orange-hrm").then((data)=>{
+      const ln = new login_objects()
+      ln.setUserName(data.username)
+      ln.setPassWord(data.password)
+      ln.clickSubmit()
+      ln.verifyLogin()
+    })
   })
 })
